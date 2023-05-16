dee6a07486142dad943b354c3941e1643e35088b
Sneeth <133808995+Sneeth@users.noreply.github.com>
2023-05-16 14:22:07 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,5 @@ streamlit.text(' 🥑Milk')
 streamlit.header(' 🥑 🥑 🥑 🥑Build Your Own Chocolate')
 import pandas
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_chocolate_list)
+streamlit.dataframe(my_fruit_list)
                
