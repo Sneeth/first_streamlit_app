a2cfc05e5ccfee18faa7d250dec8bf9d2a1ea106
Sneeth <133808995+Sneeth@users.noreply.github.com>
2023-05-16 14:16:02 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.title('🥣 🥗 🐔 🥑My Parents New Healthy Diner')
+streamlit.title('My Parents New Healthy Diner')
 streamlit.header('🥗 🐔 🥑Breakfast Menu')
 streamlit.text(' 🐔 🥑diary Milk')
 streamlit.text(' 🥑Milk')
