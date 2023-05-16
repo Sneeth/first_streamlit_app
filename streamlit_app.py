1f1d9d8e659bd41de0af55cae2845b56759d23bc
Sneeth <133808995+Sneeth@users.noreply.github.com>
2023-05-16 14:21:34 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,8 @@ streamlit.title('My Parents New Healthy Diner')
 streamlit.header('🥗 🐔 🥑Breakfast Menu')
 streamlit.text(' 🐔 🥑diary Milk')
 streamlit.text(' 🥑Milk')
+streamlit.header(' 🥑 🥑 🥑 🥑Build Your Own Chocolate')
+import pandas
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_chocolate_list)
                
